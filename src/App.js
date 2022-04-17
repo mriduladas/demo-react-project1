4792c0243e6b5abc5e19d9b176c9c1a7e3c19bc3
mriduladas <mridula199@gmail.com>
2022-04-17 10:59:52 +0530

    Working App with Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Fragment } from 'react';
 
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-  return (
-    <div>
+  const [usersList, setUsersList] = useState([]);
 
-    </div>
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </ Fragment>
   );
 }
 
